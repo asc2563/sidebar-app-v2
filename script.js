00d862639bc5e1312942008a98fd7c1b804e1e40
asc2563 <allistersaintclair@outlook.com>
2024-07-23 17:02:13 -0700

    modified index.html and script.js in .

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function search_bing() {
     window.open(`https://bing.com/search?q=${search}`);
   }
 }
+
+function goToWebsite1() {
+  var selectBox = document.getElementById("rammy");
+  var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+  if (selectedValue != "") {
+    window.location = selectedValue;
+  }
+}
+
+function goToWebsite3() {
+  var selectBox = document.getElementById("holyub");
+  var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+  if (selectedValue != "") {
+    window.location = selectedValue;
+  }
+}
